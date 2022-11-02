569a3992b53ca1bd84d43754bc2891bb5953e7d9
prashant6311 <prashantppatil761@gmail.com>
2022-11-02 14:13:00 +0530

    added all to 3 one generic method

--- generic/Generic.java
+++ generic/Generic.java
@@ -1,39 +1,9 @@
 package generic;
 
 public class Generic {
-	public static int maxIntNumber(Integer num1, Integer num2, Integer num3) {
-		if (num1.compareTo(num2) > 0) {
-			if (num1.compareTo(num3) > 0) {
-				return num1;
-			} else {
-				return num3;
-			}
-		} else {
-			if (num2.compareTo(num3) > 0) {
-				return num2;
-			} else {
-				return num3;
-			}
-		}
-	}
 
-	public static Float maxFloatNumber(Float num1, Float num2, Float num3) {
-		if (num1.compareTo(num2) > 0) {
-			if (num1.compareTo(num3) > 0) {
-				return num1;
-			} else {
-				return num3;
-			}
-		} else {
-			if (num2.compareTo(num3) > 0) {
-				return num2;
-			} else {
-				return num3;
-			}
-		}
-	}
+	static <T extends Comparable<T>> T maxValue(T num1, T num2, T num3) {
 
-	public static String maxString(String num1, String num2, String num3) {
 		if (num1.compareTo(num2) > 0) {
 			if (num1.compareTo(num3) > 0) {
 				return num1;
@@ -53,15 +23,15 @@ public class Generic {
 
 		System.out.println("Greatest of 3 number ");
 		Integer num1 = 50, num2 = 100, num3 = 20;
-		Integer max = maxIntNumber(num1, num2, num3);
+		Integer max = maxValue(num1, num2, num3);
 		System.out.println("Max number between " + num1 + ", " + num2 + " and " + num3 + " is " + max);
 
 		Float numF1 = 50.6f, numF2 = 100.9f, numF3 = 20.9f;
-		Float maxFloat = maxFloatNumber(numF1, numF2, numF3);
+		Float maxFloat = maxValue(numF1, numF2, numF3);
 		System.out.println("Max number between " + numF1 + ", " + numF2 + " and " + numF3 + " is " + maxFloat);
 
 		String str1 = "hello", str2 = "yahoo", str3 = "goat";
-		String maxString = maxString(str1, str2, str3);
+		String maxString = maxValue(str1, str2, str3);
 		System.out.println("Max String among--> " + str1 + ", " + str2 + " and " + str3 + " is " + maxString);
 
 	}
